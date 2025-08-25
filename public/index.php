95a2827565d86f4e148ffdabb4859e31163a4f52
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2025-08-25 15:39:45 +0200

    Cleanup

--- public/index.php
+++ public/index.php
@@ -7,4 +7,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $boot = new Bootstrap();
 $render = new Render();
-echo $render->render('h1', 'Hello PHP Bergen');
+$page = $render->render('h1', 'Hello PHP Bergen');
+
+print ($page);
+
