8e40b0f1cabb37126d7d51093c7f43da947467cf
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2025-08-25 18:17:44 +0200

    fix tricks

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
 
-use phpbergen\app\Kernel;
+use phpbergen\app\Bootstrap;
+use phpbergen\app\Render;
 
-require_once __DIR__ . '/../vendor/autoload_runtime.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+$boot = new Bootstrap();
+$render = new Render();
+$page = $render->render('h1', 'Hello PHP Bergen');
+
+print ($page);
